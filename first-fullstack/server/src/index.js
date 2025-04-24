9ffa9685504508ec1c12474e0c8f43f041269d57
sibshahz <shahid5ssg@gmail.com>
2025-04-24 10:48:31 +0500

    crud operations applied

--- first-fullstack/server/src/index.js
+++ first-fullstack/server/src/index.js
@@ -24,7 +24,60 @@ app.get("/", (req, res) => {
 app.get("/products", async (req, res) => {
   try {
     const products = await Product.find({}).exec();
-    res.status(200).send(products);
+    res.status(200).send({
+      myproducts: products,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const product = await Product.find({ _id: id }).exec();
+    if (product.length === 0) {
+      res.status(404).send({
+        message: "product not found",
+      });
+    }
+    res.status(200).send({
+      foundproduct: product,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send({ message: "internal server error" });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.deleteOne({
+      _id: id,
+    }).exec();
+    res.status(201).send({
+      message: `Product with id: ${id} deleted successfully`,
+    });
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const newName = req.body.name;
+    const newPrice = req.body.price;
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      { name: newName, price: newPrice },
+      { new: true }
+    );
+    res.status(201).send({ productUpdated: updatedProduct });
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: "Internal server error" });
@@ -33,8 +86,10 @@ app.get("/products", async (req, res) => {
 
 app.post("/products", async (req, res) => {
   try {
-    const { name, price, irrelevant } = req.body;
-    const newProduct = new Product({ name, price, irrelevant });
+    const name = req.body.name;
+    const price = req.body.price;
+
+    const newProduct = new Product({ name, price });
     await newProduct.save();
     res.status(201).send(newProduct);
   } catch (error) {
