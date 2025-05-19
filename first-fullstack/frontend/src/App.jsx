5bb9484534991b20b0d9192894670f5456609c6f
sibshahz <shahid5ssg@gmail.com>
2025-05-19 10:43:00 +0500

    challenge task - grade for group of husnains

--- first-fullstack/frontend/src/App.jsx
+++ first-fullstack/frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import AddProductForm from "./components/add-product";
+import { useCookies } from "react-cookie";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [cookies, setCookie] = useCookies(["token"]);
+
+  useEffect(() => {
+    console.log(cookies);
+    console.log("TOKEN", cookies);
+    if (!cookies) {
+      // window.location.href = "/signin";
+      console.log("Redirect to signin");
+    }
+  }, []);
+
   async function getData() {
     const url = "http://localhost:8000/products";
     const response = await fetch(url, {
--- first-fullstack/frontend/src/main.jsx
+++ first-fullstack/frontend/src/main.jsx
@@ -6,16 +6,19 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import SignUp from "./components/sign-up.jsx";
 import SignIn from "./components/sign-in.jsx";
 import Navigation from "./components/navigation.jsx";
+import { CookiesProvider } from "react-cookie";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-        <Route path="/dashboard" element={<App />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <CookiesProvider defaultSetOptions={{ path: "/" }}>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/dashboard" element={<App />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
   </StrictMode>
 );
